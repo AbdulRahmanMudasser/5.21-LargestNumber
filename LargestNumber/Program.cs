944c251047212494d580641e6a9c190ea79e0a1b
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-03-31 15:14:49 +0500

    solution done

--- LargestNumber/Program.cs
+++ LargestNumber/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class LargestNumber
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        int input;
+        int largestNumber = 0;
+        int counter = 0;
+
+        do
+        {
+            // incrementing counter
+            counter++;
+
+            // input first number
+            Console.Write("Input " + counter + " Number:  ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            // checking for largest number
+            if (largestNumber < input)
+            {
+                largestNumber = input;
+            }
+
+        } while (counter < 10);
+
+        // displaying the largest number
+        Console.WriteLine("Largest Number:  " + largestNumber);
+    }
+}
